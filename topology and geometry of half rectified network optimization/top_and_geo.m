f3ba47983eda590140daaa3fd26a442b57b38b2b
Deniz Sargun <denizsargun@gmail.com>
2018-12-03 20:56:00 -0500

    Update top_and_geo.m

--- topology and geometry of half rectified network optimization/top_and_geo.m
+++ topology and geometry of half rectified network optimization/top_and_geo.m
@@ -3,6 +3,7 @@ classdef top_and_geo < handle
     properties
         netSize
         nn
+        nn2
         regPen
         data
         thr
@@ -24,10 +25,9 @@ classdef top_and_geo < handle
         
         function [feat, tar] = generate_data(obj,numberOfSamples)
             % function approximation task, y = 1/2*x
-            x = mvnrnd(zeros(numberOfSamples,1),1)';
-            y = x;
-            feat = mvnrnd(y',1)';
-            tar = y;
+            d = mvnrnd(zeros(numberOfSamples,2),[1 .5; .5 1])';
+            feat = d(:,1);
+            tar = d(:,2);
         end
         
         % oracle loss
@@ -43,6 +43,18 @@ classdef top_and_geo < handle
             loss = empLoss+obj.regPen*regLoss;
         end
         
+        function loss = o_loss2(obj)
+            numberOfSamples = 1e6;
+            [feat, tar] = obj.generate_data(numberOfSamples);
+            empLoss = 1/numberOfSamples*norm(obj.nn2(feat)-tar,2);
+            regLoss = sum(sum(abs(obj.nn2.IW{1}))) ...
+                +sum(sum(abs(obj.nn2.IW{2}))) ...
+                +sum(sum(abs(obj.nn2.LW{1}))) ...
+                +sum(sum(abs(obj.nn2.LW{2}))) ...
+                +sum(abs(obj.nn2.b{1}))+sum(abs(obj.nn2.b{2}));
+            loss = empLoss+obj.regPen*regLoss;
+        end
+        
         % empirical loss
         function loss = e_loss(obj)
             numberOfSamples = length(obj.data{2});
@@ -52,6 +64,17 @@ classdef top_and_geo < handle
                 +sum(sum(abs(obj.nn.LW{1}))) ...
                 +sum(sum(abs(obj.nn.LW{2}))) ...
                 +sum(abs(obj.nn.b{1}))+sum(abs(obj.nn.b{2}));
+            loss = empLoss+obj.regPen*regLoss;
+        end
+        
+        function loss = e_loss2(obj)
+            numberOfSamples = length(obj.data{2});
+            empLoss = 1/numberOfSamples*norm(obj.nn2(obj.data{1})-obj.data{2},2);
+            regLoss = sum(sum(abs(obj.nn2.IW{1}))) ...
+                +sum(sum(abs(obj.nn2.IW{2}))) ...
+                +sum(sum(abs(obj.nn2.LW{1}))) ...
+                +sum(sum(abs(obj.nn2.LW{2}))) ...
+                +sum(abs(obj.nn2.b{1}))+sum(abs(obj.nn2.b{2}));
             loss = empLoss+obj.regPen*regLoss;
         end
         
@@ -65,13 +88,25 @@ classdef top_and_geo < handle
             obj.nn = train(obj.nn,feat,tar);
         end
         
+        function train2(obj)
+            [feat, tar] = generate_data(obj,1e6);
+            % deeper network options
+%             options = trainingOptions('sgdm', ...
+%                 'ValidationFrequency', 1e6, ...
+%                 'MiniBatchSize',16, ...
+%                 'L2Regularization',0.001);
+            obj.nn2 = train(obj.nn2,feat,tar);
+        end
+        
         function pick_two(obj)
             obj.nn = init(obj.nn);
+            obj.nn2 = obj.nn;
             obj.train;
             obj.e_loss()
             obj.o_loss()
-            nn = init(obj.nn);
-            obj.train;
+            obj.train2;
+            obj.e_loss2()
+            obj.o_loss2()
         end
         
     end
